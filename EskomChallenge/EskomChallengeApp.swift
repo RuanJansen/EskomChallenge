b492d0eb9161577957f353a440da31aa00eb40f6
RuanJansen <ruan.d.jansen@gmail.com>
2022-10-11 10:11:17 +0200

    Initial Commit

---
+++ EskomChallenge/EskomChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  EskomChallengeApp.swift
+//  EskomChallenge
+//
+//  Created by Ruan Jansen on 2022/10/11.
+//
+
+import SwiftUI
+
+@main
+struct EskomChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
